a49f1c275feea1e629532964688edf9e1c1d2f11
Ihor <kopach@users.noreply.github.com>
2022-09-30 13:04:27 +0200

    --wip-- [skip ci]

--- apps/my-nest-app/src/app/app.module.ts
+++ apps/my-nest-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from 'ROOT/app.controller';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
